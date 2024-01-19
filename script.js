ef8173d32b20be28f2f4c0af2fd6b5b6e68291ce
lsherman2 <logansherman1@gmail.com>
2024-01-18 18:35:15 -0600

    Defeating the dragon now calls winGame function

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ function attack() {
   if (health <= 0) {
     lose();
   } else if (monsterHealth <= 0) {
-    defeatMonster();
+    fighting === 2 ? winGame() : defeatMonster();
   }
 }
 
@@ -201,6 +201,8 @@ function defeatMonster() {
 function lose() {
   update(locations[5]);
 }
+
+
 
 function restart() {
   xp = 0;
